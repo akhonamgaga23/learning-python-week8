a83b2d6dda965d985c9d00c0a3eeeab098d87094
akhonamgaga23 <akhonamgaga23@gmail.com>
2025-07-06 00:04:49 +0200

    this is my second python work

---
+++ week8/shoppingcart.py
@@ -0,0 +1,26 @@
+#Create a shopping cart programme that will continously ask the udser for food product and the price of the product
+#Have an exit clause if the user wishes to stop adding more thingfs to their cart
+#At the end of the show the food items and total cost to the user
+
+foods=[]
+prices=[]
+total=0
+
+while True:
+    food=input("Enter food to buy or press q to quit:")
+    if food.lower== "q":
+        break
+    else:
+        price=float(input(f"Enter the price of the {food}:R"))
+        food.append(food)
+        price.append(price)
+
+print(".....YOUR CART.....")
+
+for food in foods:
+    print(food)
+    
+    for price in prices:
+        total+= price
+        print(f"Your total is:R{total}")
+        
